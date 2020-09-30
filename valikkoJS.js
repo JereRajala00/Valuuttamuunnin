ec98bd84c0ebf16b81ef92c3b91b34f897fba06b
aleks <juusoalk@metropolia.fi>
2020-09-30 09:39:43 +0300

    Merge branch 'tiitus' of https://github.com/JereRajala00/Valuuttamuunnin
     Conflicts:
    	index.html

--- valikkoJS.js
+++ valikkoJS.js
@@ -1,11 +1,2 @@
-const currency1 = document.getElementById("euro");
-const currency2 = document.getElementById("dollar");
 
-
-function show() {
-  document.getElementById("dd1").classList.toggle("show");
-}
-function show2() {
-  document.getElementById("dd2").classList.toggle("show");
-}
 
