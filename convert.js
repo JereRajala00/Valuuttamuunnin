f46a4e5aa8ac6eb82e992f6e020306e75a8eb869
Jere Rajala <jere.rajala80@gmail.com>
2020-09-30 14:05:12 +0300

    Add function to automatically add all currencies to dropdown menus

--- convert.js
+++ convert.js
@@ -3,20 +3,26 @@
 var currencyData
 
 window.onload = function () {
-
+  getRates().then(addCurrencies);
   var convertButton = document.getElementById('convert');
   convertButton.addEventListener("click", getRates);
 }
-function getRates() {
+async function getRates() {
   var currency1 = document.getElementById('dropdown_menu1');
   var selectedValue = currency1.options[currency1.selectedIndex].value;
   fetch('https://api.exchangeratesapi.io/latest?base=' + selectedValue)
   .then(function(response) {
     return response.json();
   }).then(function(json) {
+    while (json == null) {}
+
     currencyData = json;
     console.log(currencyData);
-    convert();
+    var sum = document.getElementById('cash_to_be_converted').value;
+    parseFloat(sum);
+    if (sum.length > 0) {
+      convert();
+    }
   }).catch(function(error) {
     console.log(error);
   });
@@ -29,5 +35,17 @@ function convert() {
   var selectedValue2 = currency2.options[currency2.selectedIndex].value;
   var result = document.getElementById("result");
   result.innerHTML = sum * currencyData.rates[selectedValue2];
+}
+async function addCurrencies() {
+  console.log(currencyData.rates.length);
+  for (let i=0;i<currencyData.rates.length;i++) {
+    console.log("moi");
+    var new_option = document.createElement("option");
+    new_option.text = currencyData.rates[i];
+    console.log(currencyData.rates[1]);
+    new_option.value = currencyData.rates[i];
+    var dropdownmenu1 = document.getElementById('dropdown_menu1');
+    dropdownmenu1.appendChild(new_option);
+  }
 }
 // Get data from currencyData-variable with syntax: currencyData.rates['USD']
